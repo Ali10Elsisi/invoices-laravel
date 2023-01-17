7c06786a1042edff6e7bf72b1f6ed697e34e1a6c
Ali Elsisi <you@example.com>
2023-01-17 23:55:06 +0200

    Ali add all files

---
+++ app/Http/Controllers/InvoiceArchiveController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\invoice_archive;
+use App\Models\Invoices;
+use Illuminate\Http\Request;
+
+class InvoiceArchiveController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $invoices = invoices::onlyTrashed()->get();
+        return view('Invoices.Archive_Invoices',compact('invoices'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\invoice_archive  $invoice_archive
+     * @return \Illuminate\Http\Response
+     */
+    public function show(invoice_archive $invoice_archive)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\invoice_archive  $invoice_archive
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(invoice_archive $invoice_archive)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\invoice_archive  $invoice_archive
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        //
+        $id = $request->invoice_id;
+        $flight = Invoices::withTrashed()->where('id', $id)->restore();
+        session()->flash('restore_invoice');
+        return redirect('/invoices');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\invoice_archive  $invoice_archive
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(invoice_archive $invoice_archive)
+    {
+        //
+        $invoices = invoices::withTrashed()->where('id',$request->invoice_id)->first();
+        $invoices->forceDelete();
+        session()->flash('delete_invoice');
+        return redirect('/Archive');
+    }
+}
